334dbe28ad462fcb9c16a0831240249c475c06db
Ana Programando <anabe.valentim@gmail.com>
2022-01-04 08:15:38 -0300

    Criando métodos de extensão

--- CSharp.BancoVirtual.SistemaAgencia/ListExtensoes.cs
+++ CSharp.BancoVirtual.SistemaAgencia/ListExtensoes.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace CSharp.BancoVirtual.SistemaAgencia
 {
-    class ListExtensoes
+    // ListExtensoes para as extensões da classe List do .Net
+    // Criada a classe estática com métodos auxiliares para estender a classe List.cs e colocar o método funcionando na lista
+    // static, pois somente armazenará extensões, não possuirá estado e nem será instanciada
+    public static class ListExtensoes
     {
+        // Coletar (varrendo) todos os elementos do array itens e adicionando cada um à listaDeInteiros
+        // static, pois em uma classe estática, os membros também devem ser estáticos
+        // Chamando um método de extensão, estendendo o tipo List<int> por meio de um método estático em uma classe estática e inserindo o tipo que será estendido como argumento
+        // AdicionarVarios() é um método de extensão onde é indicada a extensão do tipo List<T> por meio da palavra reservada this antes do primeiro argumento
+        // No momento a lógica implementada em ListExtensoes.cs só funciona com List<int>, logo não funciona com qualquer lista
+        public static void AdicionarVarios(this List<int> listaDeInteiros, params int[] itens)
+        {
+            // foreach() não traz o índice, mas não é necessário aqui
+            // Varrendo item por item, atribuindo uma ação para cada um deles
+            foreach (int item in itens)
+            {
+                listaDeInteiros.Add(item);
+            }
+        }
     }
 }
--- CSharp.BancoVirtual.SistemaAgencia/Program.cs
+++ CSharp.BancoVirtual.SistemaAgencia/Program.cs
@@ -9,6 +9,19 @@ using Humanizer;
 
 namespace CSharp.BancoVirtual.SistemaAgencia
 {
+    // Garantindo que o método AdicionarVarios() seja acessado a partir da referência de uma lista, criando uma classe derivada do tipo List.cs e implementando nela o AdicionarVarios()
+    // Derivando uma classe de List<int>, implementando AdicionarVarios(), estabelecendo params como argumento e chamando Add() no escopo, que foi definido em List<int>
+    // Nesse caso de um método pontual, não é necessário criar uma classe derivada para estender o tipo List<T>, posto que em MinhaListaExtendida, somente é gerada uma classe que deriva de List<int>, para compor métodos auxiliares
+    // Resolvido em ListExtensoes.cs
+
+    //public class MinhaListaExtendida : List<int>
+    //{
+    //    public void AdicionarVarios(params int[] itens)
+    //    {
+    //        Add(1);
+    //    }
+    //}
+
     class Program
     {
         static void Main(string[] args)
@@ -25,7 +38,23 @@ namespace CSharp.BancoVirtual.SistemaAgencia
             idades.Add(10);
             idades.Add(12);
 
-            idades.AddRange
+            // Array de int utilizando a sintaxe de inicialização de array na qual não é necessário se preocupar com o número inicial de elementos, nem com a adição de item por item no array
+            // idades.AddRange(new int[] { 1, 2, 3, 9 });
+
+            // Facilitando o que foi feito com o AddRange, definindo idades sem a preocupação com a sinaxe de criar array, passar os índices e colocar colchetes ([])
+            // ListExtensoes.AdicionarVarios(idades, 10, 20, 30, 40);
+
+            // Otimizando o AdicionarVarios
+            // Em ListExtensoes.cs o primeiro argumento de AdicionarVarios() é a listaDeInteiros, porém ela é o método que representa a classe estendendida, logo está implícito o argumento utilizado, o idades
+            // Assim basta preencher a partir do segundo argumento,o params int[] itens
+            // Adicionando as idades como argumento de AdicionarVarios()
+            // AdicionarVarios() não é um método do tipo List<T> do .NET
+            // AdicionarVarios() é um método de extensão criado em ListExtensoes.cs onde é indicada a extensão do tipo List<T> por meio da palavra reservada this antes do primeiro argumento
+
+            idades.AdicionarVarios(10, 20, 30, 40);
+            // Ao escrever a referência idades, seguida por AdicionarVarios() com seus argumentos, o compilador tranformará esses dados na chamada para o método estático
+            // No argumento, será constatado this, logo nesse caso será colocada a referência usada (idades) como primeiro argumento e os números copiados e colados na sequência:
+            // ListExtensoes.AdicionarVarios(idades, 10, 20, 30, 40); --> Como na linha 45
 
             // idades.Remove(4);
 
@@ -37,6 +66,8 @@ namespace CSharp.BancoVirtual.SistemaAgencia
 
             Console.ReadLine();
         }
+
+        // O argumento params permite chamar AdicionarVarios() sem exigir a criação manual de um array
         public void AdicionarVarios(params T[] itens)
         {
             foreach (T item in itens)
