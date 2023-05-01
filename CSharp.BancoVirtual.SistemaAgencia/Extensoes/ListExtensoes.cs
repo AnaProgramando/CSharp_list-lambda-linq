f0b2a394a5a66db37fcef160750c41b041af6b6d
AnaProgramando <anabe.valentim@gmail.com>
2023-05-01 13:07:45 -0300

    upload project

---
+++ CSharp.BancoVirtual.SistemaAgencia/Extensoes/ListExtensoes.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.BancoVirtual.SistemaAgencia.Extensoes
+{
+    // ListExtensoes para as extensões da classe List do .Net
+    // Criada a classe estática com métodos auxiliares para estender a classe List.cs e colocar o método funcionando na lista
+    // static, pois somente armazenará extensões, não possuirá estado e nem será instanciada
+    // Tornando o método genérico, definindo o tipo genérico na classe com o <T>
+
+    // A classe não será mais genérica: public static class ListExtensoes<T>
+    public static class ListExtensoes
+    {
+        // Coletar (varrendo) todos os elementos do array itens e adicionando cada um à listaDeInteiros
+        // static, pois em uma classe estática, os membros também devem ser estáticos
+        // Chamando um método de extensão, estendendo o tipo List por meio de um método estático em uma classe estática e inserindo o tipo que será estendido como argumento
+
+        // AdicionarVarios() é um método de extensão onde é indicada a extensão do tipo List por meio da palavra reservada this antes do primeiro argumento
+
+        // List<> - É necessária a existência da lista, pois ela possui o método Adicionar
+        // Lista<int> - Usando o int torna possível a utilização apenas em lista de inteiros
+        // List<T> - Para uso em outros tipos de lista, como int, string e ContaCorrente
+
+        // params int[] itens - Para deixar o array com travado apenas para int
+        // params T[] itens - Deixando o array para todos os tipos
+
+        // Ao utilizar um método de extensão é necessário inserir o this no começo do primeiro argumento
+        // public static void AdicionarVarios(this List<T> lista, params T[] itens) - Vou remover o this para não aparecer que se trata de um método de extensão
+        // Tornando o método genérico adicionando o <T>, sendo o "T" uma convenção para nomear o tipo de argumento genérico
+        // É possível ter um método genérico dentro de uma classe normal
+        public static void AdicionarVarios<T>(List<T> lista, params T[] itens)
+        {
+            // foreach() não traz o índice, mas não é necessário aqui
+            // Varrendo item por item, atribuindo uma ação para cada um deles
+            // foreach (int item in itens) - Removendo o int para travar apenas com um tipo
+            foreach (T item in itens)
+            {
+                // O tipo genérico da classe ( public static class ListExtensoes<T> ), é o mesmo tipo genérico da lista ( this List<T> lista ), tornando possível chamar o método Add com o item do array ( T[] )
+                // O Add espera o T, sem determinar que precisa ser string ou int, mas que o tipo recebido no array ( T[] ) será o mesmo tipo compativel com a lista, logo será um tipo que pode ser usado no método Add
+                lista.Add(item);
+            }
+        }
+
+        // ----------------------------------------------------------------------------------------------
+
+        // Teste de sintaxe
+        // Argumento genérico T2
+        // Método de extensão de string
+        public static void TesteGenerico<T2>(this string texto)
+        {
+
+        }
+
+        // ----------------------------------------------------------------------------------------------
+
+        static void Teste()
+        {
+            // Chamando o método e testando sintaxe
+
+            // Em função da definição na classe, é necessário sempre escrever o nome dela e informar ao compilador qual tipo utilizado na classe genérica
+            // O compilador do C# sabe qual é o argumento da classe genérica ListExtensoes<int> ao definir que se trata de uma lista de int com <int>, não porque o método AdicionarVarios() é genérico, mas porque há uma classe definida como genérica, a ListExtensoes<T>
+            List<int> idades = new List<int>();
+
+            idades.Add(12);
+            idades.Add(22);
+            idades.Add(44);
+
+            // Sendo um método normal, não um de extensão, para chamar o AdicionarVarios é neessário escrever o nome da classe genérica (ListExtensoes),
+            // indicar o tipo genérico ( <int> ), pois nesse caso se trata de uma lista de inteiros, para preencher o T (em List<T> ) do argumento do método AdicionarVarios,
+            // Chamando o método AdicionarVarios
+            // (o primeiro argumento é uma lista de inteiros, o segundo argumento é um array params de inteiros)
+            // O método recebe uma lista do respectivo tipo definido como argumento, ou seja, uma lista de inteiros
+            // A chamada abaixo deixou de fazer sentido quando a classe ListExtensoes deixou de ser genérica
+            // ListExtensoes<int>.AdicionarVarios(idades, 2, 4, 6);
+
+            // Chamando o AdicionarVarios que é um método genérico, então é necessário colocar o tipo
+            // Não é preciso escrever AdicionarVarios<int>, pois o tipo genérico T do método (AdicionarVarios<T>), é o mesmo tipo T da lista (List<T> lista), logo o compilador entende que o AdicionarVarios abaixo vai virar o AdicionarVarios de tipo genérico de int
+            // () - Não é necessário preencher exatamanete o primeiro argumento, porque o primeiro argumento de um método de extensão é o objeto, a referência que está sendo extendida, no caso o "idades", então basta preencher os próximos argumentos, no caso as idades
+            // idades.AdicionarVarios(33, 66, 88, 99);
+
+            // -------------------------------------
+
+            // TesteGenerico
+
+            string ana = "Ana";
+
+            // O TesteGenerico é um método genérico, então é necessário preencher o argumento genérico
+            // É necessário preencher como <int>, pois a string não é uma classe genérica ( TesteGenerico<T2>(this string texto) ), logo ela não trará nehuma informação para o compilador que o tipo genérico da classe é o mesmo tipo genérico do método
+            ana.TesteGenerico<int>();
+
+            // -------------------------------------
+
+            // O compilador do C# sabe qual é o argumento da classe genérica ListExtensoes<string> ao definir que se trata de uma lista de int com <string>, não porque o método AdicionarVarios() é genérico, mas porque há uma classe definida como genérica, a ListExtensoes<T>
+            List<string> nomes = new List<string>();
+
+            // É possível utilizar string como argumento de nome.Add(), pois é o tipo genérico definido em List<string>
+            nomes.Add("Ana");
+            nomes.Add("Beatriz");
+
+            // Chamando o AdicionarVarios
+            // O argumento genérico é o tipo string
+            // O argumento List<T> é um List string
+            // Chamando o AdicionarVarios com uma lista de string com os nomes
+            // O mesmo para AdicionarVarios() recebe string como argumento, pois o compilador sabe que esse método aguarda esse tipo que foi definido em ListExtensoes<string>
+            // A chamada abaixo deixou de fazer sentido quando a classe ListExtensoes deixou de ser genérica
+            // ListExtensoes<string>.AdicionarVarios(nomes, "Fulano", "Beltrano");
+
+            // A ideia do método de extensão é que não seja necessário utilizar o nome de classe para fazer as chamadas de AdicionarVarios(), e fazer com que AdicionarVarios() pareça o método da respectiva classe
+            // Aqui o argumento string é carregado pelo nome da classe(ListExtensoes<string>), mas da forma abaixo, a informação não é passada, e o código não é compilado, deixando AdicionarVarios() sublinhado em vermelho
+            // Assim ao tentar criar um método de extensão em uma classe estática, o compilador apontará erro na declaração da classe
+            // Se o método de extensão AdicionarVarios() estivesse em uma classe genérica, a chamada dele dependeria dessa classe, com o tipo especificado, logo não teria sentido utilizar a sintaxe de método de extensão, por isso deletei <T> da declaração da classe ListExtensoes na linha 15, porém o compilador não saberá mais ao que o tipo <T> de List<T> e do array T[] se refere
+            // nomes.AdicionarVarios("Sicrano", "Fulana");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,100 @@
+![banner_CSharp_list-lambda-linq](https://github.com/AnaProgramando/CSharp_list-lambda-linq/blob/adcfbe1a2ed6cbb7f5f11807e14a2dc4d1386747/banner_CSharp_list-lambda-linq.png)
+----
+
+<img src="https://img.shields.io/static/v1?label=Status&message=complete&color=32CD32&style=for-the-badge"/>
+
+<p align="center"> O curso desse projeto está disponível na Plataforma da <a href="https://www.alura.com.br/" > Alura </a> </p>
+
+<p align="center">
+ <a href="#-welcome">Welcome</a> |
+ <a href="#-features">Features</a> | 
+ <a href="#-refer%C3%AAncias">Referências</a> | 
+ <a href="#-d%C3%BAvidas">Dúvidas</a> | 
+ <a href="#%EF%B8%8F-contatos">Contatos</a> | 
+ <a href="#%EF%B8%8F-autora">Autora</a>
+</p>
+
+# 🤗 Welcome
+
+Olá, seja muito bem vinda(o)! 
+
+Tive a ideia de compartilhar alguns projetos para quem tem interesse em aprender C# (C Sharp), por isso os exercícios começam bem simples e vão dificultando aos poucos para quem gostaria de iniciar na programação ou precisa melhorar as suas habilidades, e também coloquei alguns comentários para facilitar o entendimento.
+
+📚 Aproveite o código desse exercício
+
+👩‍💻 Refaça do seu jeito
+
+😉 Se tiver qualquer dúvida, me contate
+
+## ✅ Features
+
+- [X] Estudo do tipo List<T>
+- [X] Criação de método de extensão e estático
+- [X] Implementação dentro da classe estática ListExtensoes
+- [X] Uso do modificador this para indicar método de extensão
+- [X] Criação de método genérico (T)
+- [X] Criação de lista de tipo genérico (List<T>)
+- [X] Uso do mesmo tipo genérico para lista e método
+- [X] Inferido o tipo em uma chamada de método de extensão com argumento genérico
+- [X] Uso do var
+- [X] Criação de método de extensão
+- [X] Exploração da sintaxe de inicialização de listas 
+- [X] Ordenação de lista
+- [X] Uso do método Sort()
+- [X] Implementação da interface IComparable em ContaCorrente
+- [X] Uso do CompareTo()
+- [X] Definição de lógica padrão para ContaCorrente
+- [X] Implementação da interface IComparer como argumento
+- [X] Uso do método de extensão OrderBy() que faz parte do namespace Linq
+- [X] Recebimento da expressão lambda como argumento em OrderBy()
+- [X] Uso do método Where()
+
+## ✍ Referências
+
+📗 [MSDN](https://docs.microsoft.com/pt-br/?redirectedfrom=MSDN)
+<br>
+Central de documentação e aprendizado da Microsoft para desenvolvedores
+
+📒 [Documentação do C#](https://docs.microsoft.com/pt-br/dotnet/csharp/)
+<br>
+Saiba como escrever qualquer aplicativo usando a linguagem de programação C# na plataforma .NET.
+
+## ❓ Dúvidas
+
+Qualquer dúvida, interaja aqui:
+  * Faça perguntas
+  * Dê sugestões de melhoria para o projeto
+  * Compartilhe suas ideias
+  * E interaja sobre o assunto
+
+😉Lembre-se de que esta é uma comunidade que construímos juntos 💪.
+
+## ✉️ Contatos
+
+Se precisar de ajuda, entre em contato comigo 😉
+
+[<img align="left" alt="Gmail" width="80px" src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white"/>](mailto:anabe.valentim@gmail.com)
+[<img align="left" alt="LinkedIn" width="100px" src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"/>](https://www.linkedin.com/in/ana-beatriz-valentim)
+[<img align="left" alt="Beacons" width="80px" src="https://github.com/AnaProgramando/AnaProgramando/blob/31ac40741768033915a37ec0f949984bf6aad2d1/beacons_logo.png"/>](https://beacons.page/anaprogramando)
+<br>
+
+
+## 🙋‍♀️ Autora
+
+<div>
+  <img align="left" alt="Ana Valentim" width="100px" src="https://avatars.githubusercontent.com/u/31097110?v=4"/>
+</div>
+
+<br>
+✏️ Feito com ❤️ e C# (C Sharp) por <a href="https://github.com/AnaProgramando">Ana Valentim</a>.
+
+💙 Se você gostou desse projeto, dê uma ⭐ e compartilhe!
+
+
+<br><br>
+[⬆ Voltar ao top](https://github.com/AnaProgramando/CSharp_list-lambda-linq/blob/main/README.md#) <br>
+
+
+ <div>
+  <img align="center" alt="Pixel-Art" width="1000px" src="https://github.com/AnaProgramando/CSharp_list-lambda-linq/blob/6280cbac5de9103c9775ea97e84dbef82b787ca0/w.gif"/>
+</div>
